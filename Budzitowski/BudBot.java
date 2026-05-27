34a620f6622b21e5fb2673fe72adb28d600ae0b3
ConnorWilliams112 <connor.williams@nps.edu>
2026-05-27 14:14:04 -0700

    Update BudBot.java

--- Budzitowski/BudBot.java
+++ Budzitowski/BudBot.java
@@ -16,19 +16,46 @@ public class BudBot extends Bot {
         new BudBot().start();
     }
 
-    // Called when a new round is started -> initialize and do some movement
+    //To implement:
+        //Melee vs Single checker -> Scan, add ID to table
+            //If > 1, melee tactics
+            //If 1, 1v1 tactics
+        // Power level selection tree -> Aggregate score with weights
+        // Targeting/accuracy logic
+        // Movement logic -> Random walk, wall avoidance, etc.
+        // Enemy tracking -> Keep track of scanned bots and their positions
+        // Event handling -> React to being hit, hitting others
+        
+    // Pre-compute common angles
+    private static final double[] SIN_TABLE = new double[360];
+    private static final double[] COS_TABLE = new double[360];
+
+    static {
+        for (int i = 0; i < 360; i++) {
+            double radians = Math.toRadians(i);
+            SIN_TABLE[i] = Math.sin(radians);
+            COS_TABLE[i] = Math.cos(radians);
+        }
+    }
+
+    // Fast lookups
+    public double fastSin(double degrees) {
+        return SIN_TABLE[(int) degrees % 360];
+    }
+    public double fastCos(double degrees) {
+        return COS_TABLE[(int) degrees % 360];
+    }
+
+    
     @Override
     public void run() {
         // Repeat while the bot is running
         while (isRunning()) {
-            forward(100);
-            turnGunLeft(360);
-            back(100);
-            turnGunLeft(360);
+            //TO DO: Implement movement and scanning loops here
         }
     }
 
-    // We saw another bot -> fire!
+    // Other bot IDed
     @Override
     public void onScannedBot(ScannedBotEvent e) {
         fire(1);
@@ -43,4 +70,12 @@ public class BudBot extends Bot {
         // Turn 90 degrees to the bullet direction based on the bearing
         turnRight(90 - bearing);
     }
+
+    @Override
+    public void onHitWall(HitWallEvent e) {}
+
+    @Override
+    public void onHitBot(HitBotEvent e) {}
+
+    @Override
 }
