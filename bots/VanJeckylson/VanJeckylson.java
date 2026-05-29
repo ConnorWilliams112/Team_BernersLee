7dd70eeb12ca58293baf826760cbefb13d7b1968
michaeljschnabel11 <mjschnab@gmail.com>
2026-05-29 11:33:50 -0700

    VJ defeats all enemies at least once

--- bots/VanJeckylson/VanJeckylson.java
+++ bots/VanJeckylson/VanJeckylson.java
@@ -1,11 +1,16 @@
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 import dev.robocode.tankroyale.botapi.graphics.Color;
+import java.util.Random;
 
 public class VanJeckylson extends Bot {
 
     int moveDirection = 1;
     int scansSinceDirectionChange = 0;
+    int scansBeforeDirectionChange = 4;
+    int hitCount = 0;
+    boolean defensiveMode = false;
+    Random random = new Random();
 
     public static void main(String[] args) {
         new VanJeckylson().start();
@@ -32,14 +37,19 @@ public class VanJeckylson extends Bot {
 
         // Change directions sometimes
         scansSinceDirectionChange++;
-        if (scansSinceDirectionChange >= 4) {
+        if (scansSinceDirectionChange >= scansBeforeDirectionChange) {
             moveDirection *= -1;
             scansSinceDirectionChange = 0;
+            scansBeforeDirectionChange = 3 + random.nextInt(4);
         }
 
         // Pick fire power
         double firePower;
-        if (speed < 1 && distance < 300) {
+        if (speed > 4 && distance < 250) {
+            firePower = 3;
+        } else if (speed > 4) {
+            firePower = 1.5;
+        } else if (speed < 1 && distance < 300) {
             firePower = 3;
         } else if (distance < 150) {
             firePower = 2;
@@ -56,7 +66,12 @@ public class VanJeckylson extends Bot {
 
         // Predict where the enemy will be
         double bulletSpeed = 20 - 3 * firePower;
-        double time = Math.min(distance / bulletSpeed, 12);
+        double time;
+        if (speed > 4) {
+            time = Math.min(distance / bulletSpeed, 5);
+        } else {
+            time = Math.min(distance / bulletSpeed, 12);
+        }
         double futureX = e.getX();
         double futureY = e.getY();
 
@@ -78,7 +93,19 @@ public class VanJeckylson extends Bot {
 
         // Circle around the enemy
         double bearing = calcBearing(directionTo(e.getX(), e.getY()));
-        if (distance > 500) {
+        if (defensiveMode && speed > 4 && distance < 400) {
+            // Keep distance if fast enemies keep hitting us
+            turnRight(bearing + 170);
+            forward(180 * moveDirection);
+        } else if (defensiveMode && speed > 4) {
+            // Strafe fast enemies once we have space
+            turnRight(bearing + 100);
+            forward(140 * moveDirection);
+        } else if (speed > 4 && distance < 250) {
+            // Back away from fast enemies that get too close
+            turnRight(bearing + 160);
+            forward(160 * moveDirection);
+        } else if (distance > 500) {
             turnRight(bearing + 45);
             forward(90 * moveDirection);
         } else if (distance > 180) {
@@ -109,11 +136,17 @@ public class VanJeckylson extends Bot {
 
     @Override
     public void onHitByBullet(HitByBulletEvent e) {
+        // Count hits so we can adapt our movement
+        hitCount++;
+        if (hitCount >= 3) {
+            defensiveMode = true;
+        }
+
         // switch directions when hit
         moveDirection *= -1;
         var bearing = calcBearing(e.getBullet().getDirection());
         turnRight(90 - bearing + 25 * moveDirection);
-        forward(140 * moveDirection);
+        forward(110 * moveDirection);
     }
 
     @Override
@@ -126,12 +159,16 @@ public class VanJeckylson extends Bot {
 
     @Override
     public void onDeath(DeathEvent e) {
-        // TODO
+        // reset adaptive movement for next round
+        hitCount = 0;
+        defensiveMode = false;
     }
 
     @Override
     public void onWonRound(WonRoundEvent e) {
-        // TODO
+        // reset adaptive movement for next round
+        hitCount = 0;
+        defensiveMode = false;
     }
 
 }
